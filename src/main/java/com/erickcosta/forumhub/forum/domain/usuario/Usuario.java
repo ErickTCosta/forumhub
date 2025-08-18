840c190c8369ad6ac297c94215ce0613155daa72
Erick Costa <erick.lawliet@gmail.com>
2025-08-17 21:35:13 -0300

    feat(usuarios): add Usuario entity and repository

---
+++ src/main/java/com/erickcosta/forumhub/forum/domain/usuario/Usuario.java
@@ -0,0 +1,26 @@
+package com.erickcosta.forumhub.forum.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+}

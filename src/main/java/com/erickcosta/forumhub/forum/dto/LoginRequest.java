d76e2acf545e29cc25b2b8a4dd727a0f75d3c980
Erick Costa <erick.lawliet@gmail.com>
2025-08-17 21:35:34 -0300

    feat(auth): add login request and response DTOs

---
+++ src/main/java/com/erickcosta/forumhub/forum/dto/LoginRequest.java
@@ -0,0 +1,3 @@
+package com.erickcosta.forumhub.forum.dto;
+
+public record LoginRequest(String email, String senha) { }
---
+++ src/main/java/com/erickcosta/forumhub/forum/dto/LoginResponse.java
@@ -0,0 +1,3 @@
+package com.erickcosta.forumhub.forum.dto;
+
+public record LoginResponse(String token) { }
